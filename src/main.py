bd623ce6675a78a5f18f0f66e5b3aba8afcc3951
valaisu <63551725+valaisu@users.noreply.github.com>
2024-03-17 21:03:57 +0200

    Add route visualization

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import requests
 import json
 from datetime import datetime
 import time
+from PIL import Image, ImageDraw, ImageFont
 
 from secret_info import TOKEN1, STOPS, HOME, UNI
 
@@ -137,6 +138,80 @@ def parse_itineraries(data):
     return instructions, durations
 
 
+def routes_graphical_representation(data, bar_height: int = 30, bar_medium: int = 15, window_size: int = 300, padding: int = 25):
+
+    # first parse date into formats:
+    # instructions = list[list[start_time, end_time, route_name, tansport_mode]]
+    # where an element outer list is a full route and an element inner list is a bus ride / walk / etc.
+    # durations = list[start_time: str, duration: str]
+
+    itineraries = data['data']['plan']['itineraries']
+    instructions = []
+    duration_data = []
+    now = int(datetime.now().timestamp())
+    last_arrival = 0
+    for itinerary in itineraries:
+        duration = (int((itinerary['legs'][-1]['endTime'] - itinerary['legs'][0]['startTime'])/60000))
+        start = datetime.fromtimestamp(itinerary['legs'][0]['startTime'] / 1000).strftime('%H:%M')        
+        duration_data.append(f"{start}, {duration} min")
+        route_info = []
+        for leg in itinerary['legs']:
+            mode = leg['mode']
+            start_time = int(leg['startTime'] / 1000)
+            end_time = int(leg['endTime'] / 1000)
+            if (end_time > last_arrival): last_arrival = end_time
+            route_name = leg['trip']['routeShortName'] if leg['trip'] else ''
+            vehicle = [start_time, end_time, route_name, mode]
+            route_info.append(vehicle)
+        
+        instructions.append(route_info)
+    
+    # transfer to graphical instructions
+    # list[x_up_corner: int, y_up_corner: int, x_low_corner: int, y_low_corner: int, trnasport_line: str, transport_mode: str]
+    # the list is named boxes
+    time_window = last_arrival - now  # in seconds
+    element_counter = 0
+    boxes = []
+    color_dict = {'WALK': 'grey', 'BUS': 'blue', 'TRAM': 'green', 'TRAIN': 'violet'}
+    for instr in instructions:
+        for vehicle in instr:
+            start_pos_x = padding + window_size*(vehicle[0]-now)/time_window
+            end_pos_x = padding + window_size*(vehicle[1]-now)/time_window
+            start_pos_y = padding + element_counter*(bar_height+bar_medium)
+            end_pos_y = padding + element_counter*(bar_height+bar_medium) + bar_height
+            if start_pos_x < end_pos_x and start_pos_y < end_pos_y:
+                boxes.append([start_pos_x, start_pos_y, end_pos_x, end_pos_y, vehicle[2], color_dict[vehicle[3]]])
+        element_counter += 1
+
+    print(boxes)
+
+    # draw 
+    img = Image.new('RGB', (450, 300), color='white')
+    d = ImageDraw.Draw(img)
+    font = ImageFont.truetype("arial.ttf", 12)
+    
+    # borders
+    d.rounded_rectangle([padding-10, padding-10, padding+10+window_size, window_size-padding], outline='blue', width=2, radius=3)
+
+    # transport
+    for b in boxes:
+        d.rounded_rectangle([b[0], b[1], b[2], b[3]], outline=b[5], width=3, radius=3)
+        x_center = int((b[0]+b[2])/2)
+        y_center = int((b[1]+b[3])/2)
+        d.text((x_center-len(b[4])*3, y_center-6), b[4], fill=b[5], font=font)
+
+    # durations
+    for i, text in enumerate(duration_data):
+        x_pos = window_size+padding+30
+        y_pos = padding + 6 + i*(bar_height+bar_medium)
+        print(x_pos, y_pos)
+        d.text((x_pos, y_pos), text, fill='black', font=font)
+
+    img.show()
+
+
+
+
 def main():
 
     # Show timetables specific stops
@@ -156,6 +231,9 @@ def main():
         for instruction in route:
             print(instruction)
         print()
+    
+    routes_graphical_representation(raw_data)
+
 
 main()
 
